1a3e10fbe9e3cf0ca115d770746f9bea0af7be52
HarryXu497 <92757553+HarryXu497@users.noreply.github.com>
2021-11-23 20:55:14 -0500

    Update Snake.py

--- Snake/Snake.py
+++ Snake/Snake.py
@@ -49,7 +49,7 @@ BLOCK_X = WIDTH // BLOCK_SIZE
 BLOCK_Y = HEIGHT // BLOCK_SIZE
 
 # Initial segments that you start with
-INITIAL_SEGMENTS = 767
+INITIAL_SEGMENTS = 4
 
 # Score #######################################################
 score = 0
@@ -1502,7 +1502,7 @@ while restart:
     checkLevelParams()
 
     # add coordinates for the head and 3 segments
-    if not endScreen:
+    if not endScreen:  # we don't want to add segments just to clear them later
         for i in range(INITIAL_SEGMENTS):
             blocksX.append(BLOCK_X // 2)
             blocksY.append(BLOCK_Y // 2 + i)
